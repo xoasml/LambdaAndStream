1f9f546ab5508970571be4ad31b8de5aeef3cb72
TaeHoonKim <xoasml@gmail.com>
2021-02-25 23:21:53 +0900

    람다공부

---
+++ src/lambdaTheory/No02LambdaEx02.java
@@ -0,0 +1,20 @@
+package lambdaTheory;
+
+@FunctionalInterface
+interface MyFunction2 {
+    void myMethod();
+}
+
+public class No02LambdaEx02 {
+    public static void main(String[] args) {
+        MyFunction2 f = ()-> {}; // (MyFunction2)(()-> {});
+        Object obj = (MyFunction2)(()-> {});
+        String str = ((Object)(MyFunction2)(()-> {})).toString();
+
+        System.out.println(f);
+        System.out.println(obj);
+        System.out.println(str);
+        System.out.println((MyFunction2)(()->{}));
+        System.out.println(((Object)(MyFunction2)(()->{})));
+    }
+}
---
+++ src/lambdaTheory/No04LambdaEx04.java
@@ -0,0 +1,7 @@
+package lambdaTheory;
+
+public class No04LambdaEx04 {
+    public static void main(String[] args) {
+        
+    }
+}
