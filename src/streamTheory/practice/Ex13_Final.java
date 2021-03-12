a64164d7cd9083a129a9bc03f81c337184228beb
TaeHoonKim <xoasml@gmail.com>
2021-03-12 23:33:21 +0900

    Stream final

---
+++ src/streamTheory/practice/Ex13_Final.java
@@ -0,0 +1,14 @@
+package streamTheory.practice;
+
+import java.util.stream.Stream;
+
+public class Ex13_Final {
+    public static void main(String[] args) {
+        String[] strArr = {
+                "Inheritance", "Java", "Lambda", "stream",
+                "OptionalDouble", "IntStream", "count", "sum"
+        };
+
+        Stream.of(strArr).forEach(System.out::println);
+    }
+}
