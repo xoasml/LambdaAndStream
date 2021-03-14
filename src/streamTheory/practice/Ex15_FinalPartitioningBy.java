44e697477094b323d43ce49d2c17757a1331e2bf
TaeHoonKim <xoasml@gmail.com>
2021-03-15 03:25:27 +0900

    Final partitioningBy, groupingBy

--- src/streamTheory/practice/Ex15_FinalPartitioningBy.java
+++ src/streamTheory/practice/Ex15_FinalPartitioningBy.java
@@ -1,9 +1,6 @@
 package streamTheory.practice;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -56,7 +53,7 @@ class Student2 {
     enum Level {HIGH, MID, LOW} // 성적을 상, 중, 하, 세 단계로 분류
 }
 
-public class Ex15_FinalGroupPartitioningBy {
+public class Ex15_FinalPartitioningBy {
     public static void main(String[] args) {
         Student2[] stuArr = {
                 new Student2("나자바", true, 1, 1, 300),
@@ -98,16 +95,22 @@ public class Ex15_FinalGroupPartitioningBy {
         System.out.println("여학생 수 :" + stuNumBySex.get(false));
 
         System.out.println("\n3. 단순분할 + 통계(성별 1등)");
-        Map<Boolean, Optional<Student2>> topScoreBySex = Stream.of(stuArr)
+        Map<Boolean, Optional<Student2>> topScoreBySexOpt = Stream.of(stuArr)
                 .collect(Collectors.partitioningBy(Student2::isMale,
 //                        Collectors.maxBy((o1, o2) -> o1.getScore() - o2.getScore())
                         Collectors.maxBy(Comparator.comparingInt(Student2::getScore))
                 ));
 
-        System.out.println(topScoreBySex.get(true));
-        System.out.println(topScoreBySex.get(false));
-        System.out.println(topScoreBySex.get(true).get());
-        System.out.println(topScoreBySex.get(false).get());
+        System.out.println(topScoreBySexOpt.get(true));
+        System.out.println(topScoreBySexOpt.get(false));
+
+        Map<Boolean, Student2> topScoreBySex = Stream.of(stuArr)
+                .collect(Collectors.partitioningBy(Student2::isMale,
+                        Collectors.collectingAndThen(
+                                Collectors.maxBy(Comparator.comparingInt(Student2::getScore)), Optional::get
+                        )
+                ));
+
 
         System.out.println("\n 다중분할(성별 불합격자, 100점 이하)");
         Map<Boolean, Map<Boolean, List<Student2>>> failStuBySex = Stream.of(stuArr)
@@ -117,8 +120,8 @@ public class Ex15_FinalGroupPartitioningBy {
         List<Student2> failedMaleStu   = failStuBySex.get(true).get(true);
         List<Student2> failedFemaleStu = failStuBySex.get(false).get(true);
 
-        System.out.println(failedMaleStu);
-        System.out.println(failedFemaleStu);
-
+        // list.stream 을 더 많이 씀
+        failedMaleStu.forEach(System.out::println);
+        failedFemaleStu.forEach(System.out::println);
     }
 }
