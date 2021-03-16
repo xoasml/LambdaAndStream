318ba22403563d8fa1f73a77e2df7737e3b1a4ab
TaeHoonKim <xoasml@gmail.com>
2021-03-17 07:16:49 +0900

    loop Fromatting Update

--- src/streamTheory/loopFromatting/Ex08_MiddleSorted.java
+++ src/streamTheory/loopFromatting/Ex08_MiddleSorted.java
@@ -1,6 +1,13 @@
 package streamTheory.loopFromatting;
 
 public class Ex08_MiddleSorted {
+    /*      MiddleStu Class에는 아래와 같이 compareTo가 오버라이딩 되어있다.
+     *     @Override
+     *     public int compareTo(MiddleStu o) {
+     *         return o.totalScore - this.totalScore;
+     *     }
+     */
+
     public static void main(String[] args) {
         MiddleStu[] stusArr = {
                 new MiddleStu("김자바", 1, 200),
