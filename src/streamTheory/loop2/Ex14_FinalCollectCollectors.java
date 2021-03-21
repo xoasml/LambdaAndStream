ad570b3c44b3848ebf23add7e0503844ba5b542a
TaeHoonKim <xoasml@gmail.com>
2021-03-22 00:24:02 +0900

    Update Ex14_FinalCollectCollectors.java

--- src/streamTheory/loop2/Ex14_FinalCollectCollectors.java
+++ src/streamTheory/loop2/Ex14_FinalCollectCollectors.java
@@ -1,6 +1,10 @@
 package streamTheory.loop2;
 
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Ex14_FinalCollectCollectors {
     public static void main(String[] args) {
         MiddleStu[] stuArr = {
@@ -12,5 +16,43 @@ public class Ex14_FinalCollectCollectors {
                 new MiddleStu("나자바", 3, 290),
                 new MiddleStu("감자바", 3, 180)
         };
+
+        // 학생 이름만 뽑아서 List<String>에 저장
+        List<String> name = Stream.of(stuArr).map(MiddleStu::getName).collect(Collectors.toList());
+        System.out.println(name);
+
+//        MiddleStu[] miArr = Stream.of(stuArr).toArray(value -> new MiddleStu[value]);
+        MiddleStu[] miArr = Stream.of(stuArr).toArray(MiddleStu[]::new);
+        System.out.println(Arrays.toString(miArr));
+
+        // 스트림을 Map<String, Student>로 변환, 학생 이름이 Key
+        Map<String, MiddleStu> stuMap = Stream.of(stuArr)
+                .collect(Collectors.toMap(MiddleStu::getName, middleStu -> middleStu)); // toMap(Key 입력, value 입력)
+
+        // 전체 통계 - 갯수, 총합
+        long count = Stream.of(stuArr).collect(Collectors.counting()); // count
+        long totlaScore = Stream.of(stuArr).collect(Collectors.summingLong(MiddleStu::getTotalScore)); // sum
+
+        System.out.println(count);
+        System.out.println(totlaScore);
+
+        // Collectors.reducing으로 토탈 스코어 뽑기
+        totlaScore = Stream.of(stuArr).collect(Collectors.reducing(0, MiddleStu::getTotalScore, Integer::sum));
+        System.out.println(totlaScore);
+
+        // 최고 점수 뽑기
+//        Optional<MiddleStu> topScore = Stream.of(stuArr).max((o1, o2) -> o1.totalScore - o2.totalScore);
+//        Optional<MiddleStu> topScore = Stream.of(stuArr).max(Comparator.comparing(MiddleStu::getTotalScore));
+        Optional<MiddleStu> topScore = Stream.of(stuArr).collect(Collectors.maxBy(Comparator.comparing(MiddleStu::getTotalScore)));
+        System.out.println(topScore);
+
+        // 통계 모음
+        IntSummaryStatistics stat = Stream.of(stuArr).collect(Collectors.summarizingInt(MiddleStu::getTotalScore));
+        System.out.println(stat);
+        System.out.println(stat.getMax());
+
+        // 이름 문자열로 합치기
+        String stuNames = Stream.of(stuArr).map(MiddleStu::getName).collect(Collectors.joining(", ", "[", "]"));
+        System.out.println(stuNames);
     }
 }
